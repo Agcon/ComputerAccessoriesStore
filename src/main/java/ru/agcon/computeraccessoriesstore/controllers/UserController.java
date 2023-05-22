231a5f13cc7a792b836893ae059cad1d98814c81
Agcon <agconagconovich@gmail.com>
2023-05-22 04:08:17 +0300

    first

---
+++ src/main/java/ru/agcon/computeraccessoriesstore/controllers/UserController.java
@@ -0,0 +1,46 @@
+package ru.agcon.computeraccessoriesstore.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.agcon.computeraccessoriesstore.models.User;
+import ru.agcon.computeraccessoriesstore.services.UserService;
+
+import java.security.Principal;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/login")
+    public String login(Principal principal, Model model) {
+        model.addAttribute("user", userService.getUserByPrincipal(principal));
+        return "login";
+    }
+    @GetMapping("/registration")
+    public String registration(Principal principal, Model model) {
+        model.addAttribute("user", userService.getUserByPrincipal(principal));
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String createUser(User user, Model model) {
+        if (!userService.createUser(user)){
+            model.addAttribute("errorMessage", "Такой пользователь уже существует");
+            return "registration";
+        }
+        return "redirect:/login";
+    }
+
+    @GetMapping("/user/{user}")
+    public String userInfo(@PathVariable("user") User user, Model model){
+        model.addAttribute("user", user);
+        return "user-info";
+
+    }
+
+}
